9bf5bea1ce0dd7afe6deaa2b249268cd428d7d64
Kaushik Ghose <kghose@users.noreply.github.com>
2020-12-15 14:03:51 -0500

    Remove oneOf and simplify the Query definition (#7)
    API spec
    
    * Remove oneOf and simplify query definition so query tree has a single node type
    * Simplify column type to plain string
    
    Code
    
    * Update code to match new API spec.
    * Fix paths to ping test
    * Run spotless to remove spotless errors.

--- src/main/java/bio/terra/cda/app/util/QueryUtil.java
+++ src/main/java/bio/terra/cda/app/util/QueryUtil.java
@@ -1,15 +1,5 @@
 package bio.terra.cda.app.util;
 
-import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
-import com.fasterxml.jackson.databind.ObjectMapper;
-import java.io.IOException;
-import java.nio.file.Path;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Collection;
-import java.util.Collections;
-import java.util.HashMap;
-import java.util.Map;
 import java.util.Objects;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
@@ -17,19 +7,22 @@ import java.util.stream.Stream;
 public enum QueryUtil {
   _UNUSED;
 
-  interface Node {
-  }
+  interface Node {}
+
   static class Column implements Node {
     public final String name;
+
     Column(String name) {
       this.name = name;
     }
+
     public String toString() {
       if (parent() != null) {
         return "_" + name;
       }
       return name;
     }
+
     public String parent() {
       var parts = name.split("\\.");
       if (parts.length > 1) {
@@ -38,8 +31,10 @@ public enum QueryUtil {
       return null;
     }
   }
+
   static class Value implements Node {
     public final Object value;
+
     Value(Object value) {
       this.value = value;
     }
@@ -53,6 +48,7 @@ public enum QueryUtil {
       return value.toString();
     }
   }
+
   static class Condition implements Node {
     public final Node left;
     public final String operator;
@@ -110,12 +106,14 @@ public enum QueryUtil {
 
     String sql() {
       var fromClause =
-              Stream.concat(Stream.of(table),
-                      condition.columnsToUnnest()
-                              .filter(Objects::nonNull)
-                              .distinct()
-                              .map(s -> String.format("UNNEST(%1$s) AS _%1$s", s)))
-                      .collect(Collectors.joining(", "));
+          Stream.concat(
+                  Stream.of(table),
+                  condition
+                      .columnsToUnnest()
+                      .filter(Objects::nonNull)
+                      .distinct()
+                      .map(s -> String.format("UNNEST(%1$s) AS _%1$s", s)))
+              .collect(Collectors.joining(", "));
 
       return String.format("SELECT * FROM %s WHERE %s", fromClause, condition);
     }
--- src/test/java/bio/terra/cda/app/util/QueryUtilTest.java
+++ src/test/java/bio/terra/cda/app/util/QueryUtilTest.java
@@ -2,14 +2,14 @@ package bio.terra.cda.app.util;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
-import java.nio.file.Path;
 import org.junit.jupiter.api.Test;
 
 class QueryUtilTest {
 
-  static final String EXPECTED_SQL = "SELECT * FROM gdc-bq-sample.gdc_metadata.r26_clinical, UNNEST(demographic) AS _demographic, " +
-          "UNNEST(project) AS _project, UNNEST(diagnoses) AS _diagnoses " +
-          "WHERE (((_demographic.age_at_index >= 50) AND (_project.project_id = 'TCGA-OV')) AND (_diagnoses.figo_stage = 'Stage IIIC'))";
+  static final String EXPECTED_SQL =
+      "SELECT * FROM gdc-bq-sample.gdc_metadata.r26_clinical, UNNEST(demographic) AS _demographic, "
+          + "UNNEST(project) AS _project, UNNEST(diagnoses) AS _diagnoses "
+          + "WHERE (((_demographic.age_at_index >= 50) AND (_project.project_id = 'TCGA-OV')) AND (_diagnoses.figo_stage = 'Stage IIIC'))";
 
   @Test
   public void testQuery() throws Exception {
