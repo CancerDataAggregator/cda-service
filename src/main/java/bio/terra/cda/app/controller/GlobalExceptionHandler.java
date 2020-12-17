50be748511908a480de522ed3c7c4fb9d87300c7
Kaushik Ghose <kaushik.ghose@gmail.com>
2020-12-17 16:34:31 -0500

    Apply spotless

--- src/main/java/bio/terra/cda/app/controller/GlobalExceptionHandler.java
+++ src/main/java/bio/terra/cda/app/controller/GlobalExceptionHandler.java
@@ -2,11 +2,8 @@ package bio.terra.cda.app.controller;
 
 import bio.terra.cda.common.exception.ErrorReportException;
 import bio.terra.cda.generated.model.ErrorReport;
-
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Objects;
-
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
