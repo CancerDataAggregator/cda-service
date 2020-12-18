e392cdf74c469d5854d3c554ad2932425a4556cd
Phil Shapiro <pshapiro@broadinstitute.org>
2020-12-18 13:06:43 -0500

    Tweak git-push script so it only updates files that were changed. If no changes are present, nothing is pushed.

--- misc/git-push.sh
+++ misc/git-push.sh
@@ -13,11 +13,17 @@ git init
 # Adds the files in the local repository and stages them for commit.
 git add .
 
+# Set the origin
+git remote add origin git@github.com:CancerDataAggregator/cda-service-python-client.git
+
 # Commits the tracked changes and prepares them to be pushed to a remote repository.
 git commit -m "$RELEASE_NOTE"
 
-# Set the origin
-git remote add origin git@github.com:CancerDataAggregator/cda-service-python-client.git
+# checkout master
+git fetch origin
+
+# Rebase our local changes on master, chosing our changes over master
+git rebase -X theirs origin/master
 
 # Pushes (Forces) the changes in the local repository up to the remote repository
 git push --force origin master 2>&1
