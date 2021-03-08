6920d160353d8ea005405defb003e12ba4350759
Phil Shapiro <pshapiro@broadinstitute.org>
2021-03-08 10:31:24 -0500

    Add support for sub-queries (using a query as the FROM clause) and also add support for NOT.

--- src/main/java/bio/terra/cda/app/util/QueryTranslator.java
+++ src/main/java/bio/terra/cda/app/util/QueryTranslator.java
@@ -5,9 +5,24 @@ import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 import java.util.stream.Stream;
 
+/**
+ * Class to translate the endpoint Query object to a Big Query query string.
+ */
 public class QueryTranslator {
 
+  /**
+   * Create a SQL query string given a table (or subquery) and a Query object.
+   *
+   * @param table the table or subquery string to use as the first element of the FROM clause
+   * @param query the Query object
+   * @return a SQL query string
+   */
   public static String sql(String table, Query query) {
+    if (query.getNodeType() == Query.NodeTypeEnum.SUBQUERY) {
+      // A SUBQUERY is built differently from other queries. The FROM clause is the SQL version of
+      // the query in R, instead of using table. The L subtree is now the top level query.
+      return sql(String.format("(%s)", sql(table, query.getR())), query.getL());
+    }
     var fromClause =
         Stream.concat(Stream.of(table), getUnnestColumns(query).distinct())
             .collect(Collectors.joining(", "));
@@ -20,6 +35,7 @@ public class QueryTranslator {
     switch (query.getNodeType()) {
       case QUOTED:
       case UNQUOTED:
+      case NOT:
         return Stream.empty();
       case COLUMN:
         var parts = query.getValue().split("\\.");
@@ -46,6 +62,8 @@ public class QueryTranslator {
           return String.format("_%s.%s", parts[parts.length - 2], parts[parts.length - 1]);
         }
         return query.getValue();
+      case NOT:
+        return String.format("(%s %s)", query.getNodeType(), queryString(query.getL()));
       default:
         return String.format(
             "(%s %s %s)",
