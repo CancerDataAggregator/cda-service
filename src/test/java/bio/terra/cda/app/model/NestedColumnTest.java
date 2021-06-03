81cea930a6aa9e58c8590558c21a7fc67ccee42d
Fred Kaufman <fkaufman@asymmetrik.com>
2021-06-03 12:01:07 -0400

    review changes

---
+++ src/test/java/bio/terra/cda/app/model/NestedColumnTest.java
@@ -0,0 +1,16 @@
+package bio.terra.cda.app.model;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import org.junit.jupiter.api.Test;
+
+
+public class NestedColumnTest {
+
+	@Test
+	  public void testGeneratedUnnestClause() throws Exception {
+		String nestedColumn = "ResearchSubject.indentifer.value";
+		
+
+	   // assertEquals(expectedSql, translatedQuery);
+	  }
+}
