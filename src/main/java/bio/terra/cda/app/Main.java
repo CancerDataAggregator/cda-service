555eb06c83b1cb5a887329781b203e07d2b93c98
Dion Boles <82461519+dionboles-asym@users.noreply.github.com>
2021-07-06 10:31:38 -0400

    Added a check  in MetaApiController to check table exist in bigquery (#87)
    * Added a check  in MetaApiController to check table exist in bigquery
    
    * Added a check  in MetaApiController to check table exist in bigquery and added a 15 min Scheduler to update spring cache
    
    * shorten cache time to 2 mins
    
    * removed the unnecessary logs and updated error messages
    
    * Forgot to run java spotcheck

--- src/main/java/bio/terra/cda/app/Main.java
+++ src/main/java/bio/terra/cda/app/Main.java
@@ -3,10 +3,14 @@ package bio.terra.cda.app;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
 @ComponentScan(basePackages = "bio.terra.cda")
+@EnableScheduling
+@EnableCaching
 public class Main {
   public static void main(String[] args) {
     SpringApplication.run(Main.class, args);
--- src/main/java/bio/terra/cda/app/controller/MetaApiController.java
+++ src/main/java/bio/terra/cda/app/controller/MetaApiController.java
@@ -1,12 +1,12 @@
 package bio.terra.cda.app.controller;
 
 import bio.terra.cda.app.configuration.ApplicationConfiguration;
+import bio.terra.cda.app.service.QueryService;
 import bio.terra.cda.generated.controller.MetaApi;
 import bio.terra.cda.generated.model.DatasetDescription;
 import bio.terra.cda.generated.model.DatasetInfo;
 import bio.terra.cda.generated.model.Model;
 import bio.terra.cda.generated.model.SystemStatus;
-import bio.terra.cda.generated.model.SystemStatusSystems;
 import java.time.LocalDate;
 import java.time.LocalTime;
 import java.time.OffsetDateTime;
@@ -14,14 +14,14 @@ import java.time.ZoneOffset;
 import java.util.Collections;
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 
 @Controller
 public class MetaApiController implements MetaApi {
-
   private final ApplicationConfiguration applicationConfiguration;
+
+  @Autowired public QueryService queryService;
 
   @Autowired
   public MetaApiController(ApplicationConfiguration applicationConfiguration) {
@@ -30,15 +30,7 @@ public class MetaApiController implements MetaApi {
 
   @Override
   public ResponseEntity<SystemStatus> serviceStatus() {
-    HttpStatus httpStatus = HttpStatus.OK;
-
-    SystemStatusSystems otherSystemStatus =
-        new SystemStatusSystems().ok(true).addMessagesItem("everything is fine");
-
-    SystemStatus systemStatus =
-        new SystemStatus().ok(true).putSystemsItem("BigQuery", otherSystemStatus);
-
-    return new ResponseEntity<>(systemStatus, httpStatus);
+    return ResponseEntity.ok(queryService.bigQueryCheck());
   }
 
   // For now, the dataset description is hardcoded. In the future, it will probably be read from a
