67ad6ff84993a2d17b280f9617bca37460093523
fkaufman-asym <fkaufman@asymmetrik.com>
2021-10-20 13:25:04 -0400

    integration into master merge

--- src/test/java/bio/terra/cda/app/controller/QueryApiControllerTest.java
+++ src/test/java/bio/terra/cda/app/controller/QueryApiControllerTest.java
@@ -66,13 +66,14 @@ class QueryApiControllerTest {
     String system = "GDC";
     String body = "sex";
     String tableName = "TABLE";
+    
     var expected =
         "SELECT DISTINCT sex FROM TABLE.v3, UNNEST(ResearchSubject) AS _ResearchSubject, UNNEST(_ResearchSubject.identifier) AS _identifier WHERE _identifier.system = 'GDC'";
     var result =
         mvc.perform(
                 post("/api/v1/unique-values/{version}", version)
                     .param("system", system)
-                    .param("table name", tableName)
+                    .param("table", table)
                     .contentType(MediaType.valueOf("text/plain"))
                     .content(body)
                     .accept(MediaType.APPLICATION_JSON))
