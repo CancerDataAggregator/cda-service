eb4d6598b28e02051c14e2c522dc53acd8a8c7ab
Frederick Kaufman <84089414+fkaufman-asym@users.noreply.github.com>
2021-12-01 13:19:18 -0500

    fix some easy code smells (#131)
    * fix some easy code smells
    
    * clean up code

--- src/main/java/bio/terra/cda/app/service/exception/BadQueryException.java
+++ src/main/java/bio/terra/cda/app/service/exception/BadQueryException.java
@@ -3,10 +3,6 @@ package bio.terra.cda.app.service.exception;
 import bio.terra.cda.common.exception.BadRequestException;
 import java.util.List;
 
-// TODO: TEMPLATE: In general, prefer specific exceptions to general exceptions.
-//  The PingService throws this exception. It is caught by the global exception
-//  handler and turned into an ErrorReport response.
-
 public class BadQueryException extends BadRequestException {
 
   public BadQueryException(String message) {
--- src/main/java/bio/terra/cda/common/exception/UnauthorizedException.java
+++ src/main/java/bio/terra/cda/common/exception/UnauthorizedException.java
@@ -10,23 +10,23 @@ import org.springframework.http.HttpStatus;
 public abstract class UnauthorizedException extends ErrorReportException {
   private static final HttpStatus thisStatus = HttpStatus.UNAUTHORIZED;
 
-  public UnauthorizedException(String message) {
+  protected UnauthorizedException(String message) {
     super(message, null, thisStatus);
   }
 
-  public UnauthorizedException(String message, Throwable cause) {
+  protected UnauthorizedException(String message, Throwable cause) {
     super(message, cause, null, thisStatus);
   }
 
-  public UnauthorizedException(Throwable cause) {
+  protected UnauthorizedException(Throwable cause) {
     super(null, cause, null, thisStatus);
   }
 
-  public UnauthorizedException(String message, List<String> causes) {
+  protected UnauthorizedException(String message, List<String> causes) {
     super(message, causes, thisStatus);
   }
 
-  public UnauthorizedException(String message, Throwable cause, List<String> causes) {
+  protected UnauthorizedException(String message, Throwable cause, List<String> causes) {
     super(message, cause, causes, thisStatus);
   }
 }
