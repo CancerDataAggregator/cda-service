7bcd02d8f024854e84eae1375c344401588386ed
rfricke-asymmetrik <rfricke@asymmetrik.com>
2022-03-01 14:26:28 -0500

    Remove some methods

--- src/main/java/bio/terra/cda/app/generators/CountsSqlGenerator.java
+++ src/main/java/bio/terra/cda/app/generators/CountsSqlGenerator.java
@@ -20,21 +20,6 @@ public class CountsSqlGenerator extends SqlGenerator {
                 return sql(qualifiedTable, rootQuery);
         }
 
-        private Stream<String> getUnnestsFromParts(String[] parts, Boolean includeLast) {
-                return IntStream.range(0, parts.length - (includeLast ? 0 : 1))
-                                .mapToObj(
-                                                i -> i == 0
-                                                                ? String.format("UNNEST(%1$s.%2$s) AS %3$s", table,
-                                                                                parts[i], getAlias(i, parts))
-                                                                : String.format("UNNEST(%1$s.%2$s) AS %3$s",
-                                                                                getAlias(i - 1, parts), parts[i],
-                                                                                getAlias(i, parts)));
-        }
-
-        private String getAlias(Integer index, String[] parts) {
-                return "_" + Arrays.stream(parts, 0, index + 1).collect(Collectors.joining("_"));
-        }
-
         private String sql(String tableOrSubClause, Query query) {
                 if (query.getNodeType() == Query.NodeTypeEnum.SUBQUERY) {
                         // A SUBQUERY is built differently from other queries. The FROM clause is the
