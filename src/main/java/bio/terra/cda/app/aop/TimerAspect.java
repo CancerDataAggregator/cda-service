9a07dd47af762a051df510c46091a246c6618070
Frederick Kaufman <84089414+fkaufman-asym@users.noreply.github.com>
2022-04-01 11:14:04 -0400

    codeSmells (#165)
    * codeSmells
    
    * code smells

--- src/main/java/bio/terra/cda/app/aop/TimerAspect.java
+++ src/main/java/bio/terra/cda/app/aop/TimerAspect.java
@@ -1,6 +1,7 @@
 package bio.terra.cda.app.aop;
 
 import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.Signature;
 import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
 import org.slf4j.Logger;
@@ -18,8 +19,8 @@ public class TimerAspect {
     long start = System.currentTimeMillis();
     Object proceed = joinPoint.proceed();
     long executionTime = System.currentTimeMillis() - start;
-    logger.info(
-        "--Execution Timer: " + joinPoint.getSignature() + " executed in " + executionTime + "ms");
+    Signature signature = joinPoint.getSignature();
+    logger.info("--Execution Timer: {} executed in {}ms", signature, executionTime);
     return proceed;
   }
 }
