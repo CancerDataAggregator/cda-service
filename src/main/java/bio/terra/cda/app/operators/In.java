43c838da73a548c6a8d11dc786b8c2486e214f8f
rfricke-asymmetrik <rfricke@asymmetrik.com>
2022-04-11 13:17:29 -0400

    add case insensitivity for in statement

--- src/main/java/bio/terra/cda/app/operators/In.java
+++ src/main/java/bio/terra/cda/app/operators/In.java
@@ -2,7 +2,10 @@ package bio.terra.cda.app.operators;
 
 import bio.terra.cda.app.util.TableSchema;
 import bio.terra.cda.generated.model.Query;
+
+import java.util.Arrays;
 import java.util.Map;
+import java.util.stream.Collectors;
 
 @QueryOperator(nodeType = Query.NodeTypeEnum.IN)
 public class In extends SingleSidedOperator {
@@ -11,7 +14,15 @@ public class In extends SingleSidedOperator {
       throws IllegalArgumentException {
     String right = ((BasicOperator) getR()).queryString(table, tableSchemaMap);
     if (right.contains("[") || right.contains("(")) {
-      right = right.substring(1, right.length() - 1).replace("\"", "'");
+      right = Arrays.stream(
+                right.substring(1, right.length() - 1).replace("\"", "'").split(",")
+              ).map(value -> {
+                if (value.contains("'")) {
+                  return String.format("UPPER(%s)", value);
+                }
+
+                return value;
+              }).collect(Collectors.joining(", "));
     } else {
       throw new IllegalArgumentException("To use IN you need to add [ or (");
     }
