a40d694315dc1d4bc861874cb8a645c0105d25d3
rfricke-asymmetrik <rfricke@asymmetrik.com>
2022-09-01 10:12:18 -0400

    dont lowercase count aliases

--- src/main/java/bio/terra/cda/app/generators/EntityCountSqlGenerator.java
+++ src/main/java/bio/terra/cda/app/generators/EntityCountSqlGenerator.java
@@ -81,7 +81,7 @@ public class EntityCountSqlGenerator extends SqlGenerator {
         .distinct()
         .map(
             field -> {
-              String name = field.equals(finalPartitionKeyField) ? "total" : field.toLowerCase();
+              String name = field.equals(finalPartitionKeyField) ? "total" : field;
 
               String fieldToUse = field;
               TableInfo tableInfo = this.entityTable;
