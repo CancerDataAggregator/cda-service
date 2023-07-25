5b3e81c6be4ce3dd418fe75d1b234383737a28a3
Andrea Haessly <ahaessly@broadinstitute.org>
2023-07-25 18:27:29 -0400

    AH - test BEE (#218)
    * add app local properties
    * chnage to use sql proxy
    * change version to 3.2.0, remove bq build tasks

--- src/main/java/bio/terra/cda/app/configuration/ApplicationConfiguration.java
+++ src/main/java/bio/terra/cda/app/configuration/ApplicationConfiguration.java
@@ -2,22 +2,18 @@ package bio.terra.cda.app.configuration;
 
 import bio.terra.cda.app.operators.QueryModule;
 import bio.terra.cda.app.models.RdbmsSchema;
-import bio.terra.cda.app.service.TablePrecedenceComparator;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.datatype.jdk8.Jdk8Module;
 import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 import com.fasterxml.jackson.module.paramnames.ParameterNamesModule;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.context.properties.ConfigurationProperties;
-import org.springframework.cloud.gcp.autoconfigure.sql.GcpCloudSqlAutoConfiguration;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Component;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 import java.util.Arrays;
-import java.util.Comparator;
 import java.util.List;
 
 @Component
@@ -34,17 +30,13 @@ public class ApplicationConfiguration {
     return version;
   }
 
+
   @Value("${cda.table-precedence}")
   private String[] tablePrecedence;
 
   public List<String> getTablePrecedence() {
     return Arrays.asList(tablePrecedence);
   }
-
-//  @Bean
-//  public CacheManager cacheManager() {
-//    return new ConcurrentMapCacheManager("system-status");
-//  }
 
   @Bean("objectMapper")
   public ObjectMapper objectMapper() {
@@ -54,7 +46,6 @@ public class ApplicationConfiguration {
         .registerModule(new JavaTimeModule())
         .registerModule(new QueryModule());
   }
-
 
   @Bean()
   public RdbmsSchema rdbmsSchema() { return new RdbmsSchema();}
