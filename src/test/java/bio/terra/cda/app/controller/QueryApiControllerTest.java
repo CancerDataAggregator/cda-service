70b4c1cb25ada2a6654006807f19fc0c5beb07e1
Andrea Haessly <ahaessly@broadinstitute.org>
2023-11-17 16:20:46 -0500

    fix unique_terms querys when filtering on system

--- src/test/java/bio/terra/cda/app/controller/QueryApiControllerTest.java
+++ src/test/java/bio/terra/cda/app/controller/QueryApiControllerTest.java
@@ -53,7 +53,7 @@ class QueryApiControllerTest {
             });
 
     var expected =
-        "SELECT DISTINCT sex FROM subject LEFT JOIN subject_identifier AS subject_identifier ON subject.id = subject_identifier.subject_id WHERE system = 'GDC' ORDER BY sex  LIMIT 100";
+        "SELECT DISTINCT sex FROM subject WHERE id IN (SELECT DISTINCT(subject_id) FROM subject_identifier WHERE system = 'GDC') ORDER BY sex  LIMIT 100";
     var result =
         mvc.perform(
                 post("/api/v1/unique-values")
