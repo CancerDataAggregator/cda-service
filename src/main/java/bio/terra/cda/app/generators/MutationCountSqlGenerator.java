69c21c307de5801e24505ece295dc74f29bd45fb
Andrea Haessly <ahaessly@broadinstitute.org>
2023-12-14 13:44:04 -0500

    change field names when specifying fields in the mapping tables

--- src/main/java/bio/terra/cda/app/generators/MutationCountSqlGenerator.java
+++ src/main/java/bio/terra/cda/app/generators/MutationCountSqlGenerator.java
@@ -4,8 +4,8 @@ import bio.terra.cda.generated.model.Query;
 import java.io.IOException;
 
 @CountQueryGenerator(
-    entity = "mutation",
-    totalFieldsToCount = {"id"},
+    entity = "somatic_mutation",
+    totalFieldsToCount = {"cda_subject_id"},
     groupedFieldsToCount = {
       "project_short_name",
       "NCBI_Build",
