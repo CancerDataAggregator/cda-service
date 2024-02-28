4fce0d61fb9d549340767e7ac74b070dc426cb0b
Andrea Haessly <ahaessly@broadinstitute.org>
2024-02-28 10:59:52 -0500

    add boolean types for parameters

--- src/main/java/bio/terra/cda/app/builders/ParameterBuilder.java
+++ src/main/java/bio/terra/cda/app/builders/ParameterBuilder.java
@@ -2,18 +2,18 @@ package bio.terra.cda.app.builders;
 
 import bio.terra.cda.app.models.QueryField;
 import org.apache.logging.log4j.util.Strings;
-import org.springframework.jdbc.core.SqlParameterValue;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;import org.springframework.jdbc.core.SqlParameterValue;
 import org.springframework.jdbc.core.namedparam.MapSqlParameterSource;
 import org.springframework.util.StringUtils;
 
 import java.sql.Types;
 import java.util.Arrays;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 import java.util.stream.Collectors;
 
 public class ParameterBuilder {
+  private static final Logger logger = LoggerFactory.getLogger(ParameterBuilder.class);
   private final MapSqlParameterSource parameterValueMap;
   private int index;
 
@@ -28,15 +28,20 @@ public class ParameterBuilder {
 
   public String addParameterValue(String type, Object value) {
     String parameterName = String.format("parameter_%s", ++index);
+
     if (value.getClass().isArray()) {
       this.parameterValueMap.addValue(parameterName, value, Types.ARRAY);
-    } else
-    if (type.equals("text")) {
+    } else if (type.equals("text")) {
       this.parameterValueMap.addValue(parameterName, value);
-    } else if (type.equals("integer")){
+    } else if (type.equals("integer")) {
       this.parameterValueMap.addValue(parameterName, value, Types.INTEGER);
     } else if (type.equals("float")) {
       this.parameterValueMap.addValue(parameterName, value, Types.FLOAT);
+    } else if (type.equals("boolean")) {
+      this.parameterValueMap.addValue(parameterName, value, Types.BOOLEAN);
+    } else {
+      logger.error("Unknown type: {}. Trying to add anyway", type);
+      this.parameterValueMap.addValue(parameterName, value);
     }
     return String.format(":%s", parameterName);
   }
@@ -47,7 +52,7 @@ public class ParameterBuilder {
       String keyformat = String.format(":%s", key);
       Object value = parameterValueMap.getValue(key);
       int type = parameterValueMap.getSqlType(key);
-      if (type == Types.INTEGER || type == Types.FLOAT) {
+      if (type == Types.INTEGER || type == Types.FLOAT || type == Types.BOOLEAN) {
         result = result.replace(keyformat, value.toString());
       } else if (type == Types.ARRAY) {
         List<String> valueList = Arrays.stream((Object[])value).map(x ->
