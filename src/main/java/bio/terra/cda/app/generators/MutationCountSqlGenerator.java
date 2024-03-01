a9bbb2ccb0768b1a5774eea450803ed5363ea5ce
tanner-coon-bh <tanner.coon@bluehalo.com>
2024-03-01 15:31:51 -0700

    Updated mutation count column names
    see description

--- src/main/java/bio/terra/cda/app/generators/MutationCountSqlGenerator.java
+++ src/main/java/bio/terra/cda/app/generators/MutationCountSqlGenerator.java
@@ -8,10 +8,10 @@ import java.io.IOException;
     totalFieldsToCount = {"cda_subject_id"},
     groupedFieldsToCount = {
       "project_short_name",
-      "NCBI_Build",
-      "Chromosome",
-      "Variant_Type",
-      "One_Consequence"
+      "ncbi_build",
+      "chromosome",
+      "variant_type",
+      "one_consequence"
     })
 public class MutationCountSqlGenerator extends EntityCountSqlGenerator {
   public MutationCountSqlGenerator(Query rootQuery) {
