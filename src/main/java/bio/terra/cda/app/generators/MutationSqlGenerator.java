ddc5226e81b8cf23067f53e574ab850e12800bf4
tanner-coon-bh <tanner.coon@bluehalo.com>
2024-06-06 09:58:57 -0600

    Updated Mutation Default Order By
    Changed mutation default order by from case_barcode to integer_id_alias

--- src/main/java/bio/terra/cda/app/generators/MutationSqlGenerator.java
+++ src/main/java/bio/terra/cda/app/generators/MutationSqlGenerator.java
@@ -3,7 +3,7 @@ package bio.terra.cda.app.generators;
 import bio.terra.cda.generated.model.Query;
 
 // TODO - case_barcode may need to be altered
-@EntityGeneratorData(entity = "mutation", hasFiles = false, defaultOrderBy = "case_barcode",
+@EntityGeneratorData(entity = "mutation", hasFiles = false, defaultOrderBy = "mutation_integer_id_alias",
     aggregatedFields = {},
     aggregatedFieldsSelectString = {})
 public class MutationSqlGenerator extends EntitySqlGenerator {
