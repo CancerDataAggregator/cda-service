fb26efc2d52d975622ed31e5e8aadc41eac55005
tanner-coon-bh <155670848+tanner-coon-bh@users.noreply.github.com>
2024-06-24 10:35:01 -0600

    Fixed Mutation Default Order By (#260)
    Simple fix to resolve performance issues caused by ordering by non-primarykey column

--- src/main/java/bio/terra/cda/app/generators/MutationSqlGenerator.java
+++ src/main/java/bio/terra/cda/app/generators/MutationSqlGenerator.java
@@ -3,7 +3,7 @@ package bio.terra.cda.app.generators;
 import bio.terra.cda.generated.model.Query;
 
 // TODO - case_barcode may need to be altered
-@EntityGeneratorData(entity = "mutation", hasFiles = false, defaultOrderBy = "mutation_integer_id_alias",
+@EntityGeneratorData(entity = "mutation", hasFiles = false, defaultOrderBy = "mutation_id",
     aggregatedFields = {},
     aggregatedFieldsSelectString = {})
 public class MutationSqlGenerator extends EntitySqlGenerator {
